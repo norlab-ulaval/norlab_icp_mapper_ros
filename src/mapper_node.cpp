6dfc0c93d2b74cbe3a22be2fc696e4e57d9405d7
boxanm <matej.boxan@gmail.com>
2025-01-05 18:35:20 -0500

    Set deskewingScanPublisher queue size to 1

--- src/mapper_node.cpp
+++ src/mapper_node.cpp
@@ -56,7 +56,7 @@ public:
 
         mapPublisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("map", 2);
         inputFiltersScanPublisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("scan_after_input_filters", 1);
-        deskewingScanPublisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("scan_after_deskew", 2);
+        deskewingScanPublisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("scan_after_deskew", 1);
         odomPublisher = this->create_publisher<nav_msgs::msg::Odometry>("icp_odom", 50);
 
         if(params->is3D)

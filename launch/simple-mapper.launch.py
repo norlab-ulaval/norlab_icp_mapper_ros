90a8675c12cc2e7c886ecb6f25525d1ad59995fd
boxanm <matej.boxan@gmail.com>
2025-11-09 09:11:11 -0500

    Save map cells on RAM

--- launch/simple-mapper.launch.py
+++ launch/simple-mapper.launch.py
@@ -162,7 +162,7 @@ def generate_launch_description():
                 "is_mapping": IS_MAPPING,
                 "is_online": True,
                 "is_3D": True,
-                "save_map_cells_on_hard_drive": True,
+                "save_map_cells_on_hard_drive": False,
                 "publish_tfs_between_registrations": True,
                 "deskew": False,
                 "compression_voxel_size": 0.5,
